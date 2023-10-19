f9b8c8a62df9a0eec221b90892daaf94ff4d44ba
Karenandrea86 <andreahueso86@gmail.com>
2023-10-18 19:31:58 -0500

    first commit

---
+++ async_await.js
@@ -0,0 +1,13 @@
+const axios= require('axios')
+
+const url = 'https://dinosaur-facts-api.shultzlab.com/dinosaurs'
+
+async function obtenerDinos(){
+    try{
+        const respuesta = await axios.get(url);
+        console.log(respuesta.data)
+    }catch(error){
+        
+    }
+}
+obtenerDinos()
